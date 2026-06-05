65b859ab0ac5f2e508372c82dd8bfe5df895c9c1
Bidsho <bisho54@gmail.com>
2026-06-05 15:20:05 +0100

    Add service sync from JAP API

--- orders/admin.py
+++ orders/admin.py
@@ -29,18 +29,45 @@ class OrderAdmin(admin.ModelAdmin):
         return super().changelist_view(request, extra_context=extra_context)
 
 
+# In your ServiceAdmin, add a custom action:
+
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('provider_service_id', 'name', 'category', 'cost_per_1k_usd', 'is_active')
     list_editable = ('is_active',)
     list_filter = ('is_active', 'category')
     search_fields = ('name', 'category', 'provider_service_id')
+    actions = ['sync_from_jap_api']
 
-    def changelist_view(self, request, extra_context=None):
-        extra_context = extra_context or {}
-        extra_context.update(get_dashboard_metrics())
-        return super().changelist_view(request, extra_context=extra_context)
+    def sync_from_jap_api(self, request, queryset):
+        import requests
+        from django.conf import settings
 
+        response = requests.post('https://justanotherpanel.com/api/v2', data={
+            'key': settings.JAP_API_KEY,
+            'action': 'services'
+        })
+        services = response.json()
+
+        created, updated = 0, 0
+        for svc in services:
+            obj, was_created = Service.objects.update_or_create(
+                provider_service_id=svc['service'],
+                defaults={
+                    'name': svc['name'],
+                    'category': svc['category'],
+                    'cost_per_1k_usd': svc['rate'],
+                    'is_active': False,  # admin manually activates
+                }
+            )
+            if was_created:
+                created += 1
+            else:
+                updated += 1
+
+        self.message_user(request, f"Synced: {created} created, {updated} updated.")
+
+    sync_from_jap_api.short_description = "Sync services from JAP API"
 
 @admin.register(SocialAccount)
 class SocialAccountAdmin(admin.ModelAdmin):
