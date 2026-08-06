5afc8f52152ae8893fe48570f229dec79fed9ab9
Bidsho <bisho54@gmail.com>
2026-08-06 04:55:47 +0100

    Update

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class Service(models.Model):
 
     def get_naira_price(self):
         exchange_rate = Decimal('1700')
-        profit_multiplier = Decimal('1.2')
+        profit_multiplier = Decimal('1.4')
         return (self.cost_per_1k_usd * exchange_rate * profit_multiplier).quantize(Decimal('0.01'))
 
 
